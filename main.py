2740cdbef3226b50880ddcc547c1fc456af45251
RoboTuan <testingapplication96@gmail.com>
2020-11-05 17:00:41 +0100

    Added main

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 import numpy as np
 import tensorflow as tf
+import csv
+
 
 
 filename = './numbers.tfrecord'
+
 data = 0
 ora = 0
 temperatura = 0
-unidità=0
-audioFile=0
+umidità = 0
+audioFile = 0
+
+folder = "./raw_data/"
+file = folder + "samples.csv"
 
 with tf.io.TFRecordWriter(filename) as writer:
-    for i in range(10):
+    with open(file, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            #print(row, type(row))
+            row = row[0].split(",")
+            #print(row, type(row)) 
+            #print()
+            date = tf.constant(row[0]).numpy()
+            hour = tf.constant(row[1]).numpy()
+            temp = int(row[2])
+            humidity = int(row[3])
+            audioFile = tf.constant(row[4]).numpy()
 
-        # Feature≠Features
-        x_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[x[i]]))
-        y_feature = tf.train.Feature(float_list=tf.train.FloatList(value=[y[i]]))
-        
-        mapping = {'integer': x_feature,
-                    'float' : y_feature}
-        
-        example = tf.train.Example(features=tf.train.Features(feature=mapping))
-        
-        writer.write(example.SerializeToString())
-        
+            # Feature≠Features
+            date_feature = tf.train.Feature(bytes_list=tf.train.BytesList(value=[date]))
+            hour_feature = tf.train.Feature(bytes_list=tf.train.BytesList(value=[hour]))
+            temp_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[temp]))
+            humidity_feature = tf.train.Feature(int64_list=tf.train.Int64List(value=[humidity]))
+            audioFile_feature = tf.train.Feature(bytes_list=tf.train.BytesList(value=[audioFile]))
+
+            mapping = {'date': date_feature,
+                        'hour' : hour_feature,
+                        'temperature': temp_feature,
+                        'humidity': humidity_feature,
+                        'audioFile': audioFile_feature}
+            
+            example = tf.train.Example(features=tf.train.Features(feature=mapping))
+            
+            print(example)
+
+            writer.write(example.SerializeToString())
+            
 
     
